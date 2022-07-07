ea55eafb10dd3884d75b62c43818e0aace1fde9b
PrivateGiles <56416493+PrivateGiles@users.noreply.github.com>
2022-07-07 14:09:04 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean' 
+ games { 'gta5' } 
+  
+ author 'PrivateGiles' 
+ description 'Disables keys being pressed inside a vehicle' 
+ version '2.0.0' 
+  
+ client_script 'client.lua'
